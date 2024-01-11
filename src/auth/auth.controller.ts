bbc1876114ffbc15ddbce4c188da7355d2265295
Sergio Suárez <sgabriel4@gmail.com>
2024-01-11 11:16:03 -0300

    Validación JWT y obtener user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginDto, RegisterUserDto, UpdateAuthDto } from './dto';
+import { AuthGuard } from './guards/auth.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
@@ -12,17 +13,20 @@ export class AuthController {
   }
 
   @Post('/login')
-  login(@Body() loginDto: LoginDto){
+  login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
   }
 
   @Post('/register')
-  register(@Body() registerDto: RegisterUserDto){
+  register(@Body() registerDto: RegisterUserDto) {
     return this.authService.register(registerDto);
   }
 
+  @UseGuards(AuthGuard)
   @Get()
-  findAll() {
+  findAll(@Request() req: Request) {
+    // const user = req['user'];
+
     return this.authService.findAll();
   }
 
